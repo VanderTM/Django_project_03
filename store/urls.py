27e51b33b53b1b1bcc872cd7cb9694c57c5b6e05
VanderTM <pcstoregroup@gmail.com>
2025-05-07 17:31:26 -0400

    finish detail

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from store.views import ProductListView
+from store.views import *
 urlpatterns=[
 
-path("",ProductListView.as_view(),name="product_list")
-
+path("",ProductListView.as_view(),name="product_list"),
+path("<int:pk>",ProductDetailView.as_view(),name="detail")
 ]
